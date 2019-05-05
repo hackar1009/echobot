eb230f611b07935c7caf2300a8fc57efc22146bc
Echo <36556830+EchoNoahGaming@users.noreply.github.com>
2019-05-05 10:13:51 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,10 +21,4 @@ async def announcement(ctx, *, args):
 	embed.set_footer(text="By EchoNoahGaming")
 	await ctx.send("", embed=embed)
 
-async def embed(*):
-	"""Input anything, and the bot will say it back in a embed thing."""
-	embed=discord.Embed(title="Embed Message", description=args, color=0x7700aa)
-	embed.set_footer(text="By EchoNoahGaming")
-	await ctx.send("", embed=embed)
-
-client.run(str(os.environ.get('BOT_TOKEN')))
+bot.run(str(os.environ.get('BOT_TOKEN')))
